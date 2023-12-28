18746bc1a65f6869b454b7174c78c682e4879dc1
Adam Knowles <1413836+Pharkie@users.noreply.github.com>
2023-12-28 15:07:21 +0000

    added WLED test

---
+++ Departure boards micropython/test_WLED.py
@@ -0,0 +1,17 @@
+import urequests
+import ujson
+
+# Replace with your WLED device's IP address
+WLED_IP = "192.168.8.137"
+
+def send_command_to_wled(command):
+    url = f"http://{WLED_IP}/json/state"
+    headers = {"Content-Type": "application/json"}
+    data = ujson.dumps(command)
+    response = urequests.post(url, headers=headers, data=data)
+    return response.json()
+
+# Turn on the WLED device
+command = {"on": True}
+response = send_command_to_wled(command)
+print(response)
