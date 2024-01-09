9b21bedb5c4cdb16b14ecb490f55def257eaede4
Adam Knowles <1413836+Pharkie@users.noreply.github.com>
2024-01-09 22:22:47 +0000

    minor update

--- Departure boards micropython/test_WLED.py
+++ Departure boards micropython/test_WLED.py
@@ -11,7 +11,11 @@ def send_command_to_wled(command):
     response = urequests.post(url, headers=headers, data=data)
     return response.json()
 
-# Turn on the WLED device
-command = {"on": True}
+# # Turn on the WLED device
+# command = {"on": True}
+
+# Set the effect to preset 1
+command = {"ps": 1}
+
 response = send_command_to_wled(command)
 print(response)
